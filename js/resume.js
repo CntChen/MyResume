3eb058c13b6aa38e3d0246f81da34adc1aac4b40
CntChen <cntchen@gmail.com>
2015-07-31 22:11:21 +0800

    add animation

--- js/resume.js
+++ js/resume.js
@@ -1,1 +1,10 @@
 var page1 = document.getElementById('page1');
+
+document.addEventListener('scroll', handler, false);
+//document.attachEvent('onscroll', handler);
+document['onscroll'] = handler;
+
+function handler () {
+	console.log("eeeee");
+	
+}
